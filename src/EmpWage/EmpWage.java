1d8951e2b98cce760946f389fe49db5c44d36f4a
swayampandey <swayam265@gmail.com>
2022-06-29 16:14:05 +0530

    EmpWage Computation

---
+++ src/EmpWage/EmpWage.java
@@ -0,0 +1,11 @@
+package EmpWage;
+
+public class EmpWage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to employee Wage Computation");
+
+	}
+
+}
