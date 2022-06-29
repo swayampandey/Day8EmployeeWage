3cc56285dd6b2edde9da78121a9179f6a4933151
swayampandey <swayam265@gmail.com>
2022-06-29 16:30:20 +0530

    Added : UC2

--- src/EmpWage/EmpWage.java
+++ src/EmpWage/EmpWage.java
@@ -4,15 +4,19 @@ public class EmpWage {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("welcome to employee Wage Computation");
-		System.out.println();
+		System.out.println("Welcome to employee wage computation");
 		int employeePresent=1;
-		int ispresent = (int) (Math.floor(Math.random()*10)%2);
-		if(ispresent == employeePresent)
+		int wagePerhour=20;
+		int empHour=8;
+		int ispresent= (int) (Math.floor(Math.random()*10)%2);
+		if(ispresent==employeePresent) {
 			System.out.println("Employee is Present");
-		else
+		System.out.println("Employee Wage for day=" +empHour*wagePerhour);
+		} else {
 			System.out.println("Employee is Absent");
+			System.out.println("Employee Wage for day= 0");
 
 	}
 
+	}
 }
